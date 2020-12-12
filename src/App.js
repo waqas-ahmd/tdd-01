521d2c8f9b6712bac031634b7963f0d3c8f7fcc0
waqas-ahmd <65waqas@gmail.com>
2020-12-12 11:10:02 +0500

    v2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Test Driven Development</h1>
+      <h2>with CI/CD</h2>
       <input type="text" placeholder="Enter your name" />
     </div>
   );
